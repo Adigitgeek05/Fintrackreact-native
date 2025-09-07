87c0f93ac598520e678df8d2a62663daac1a3dca
Adigitgeek05 <adigitgeek@gmail.com>
2025-09-08 04:07:14 +0530

    Frontendd setup

---
+++ mobile/app/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View
+      
+    >
+      <Text style={{ color: "blue" }}>Edit app/index.tsx to edit this screen.12223</Text>
+    </View>
+  );
+}
